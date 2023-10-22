05cdc4304d6d2bc99c80d141c678d2d63f87b32d
2inlee <kbs@kakao.com>
2023-10-22 21:08:11 +0900

    make interfave Post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/**
+ * author : string;
+ * title : string;
+ * content : string;
+ * likeCount : number;
+ * commentCount : number;
+ */
+interface Post {
+  author : string;
+  title : string;
+  content : string;
+  likeCount : number;
+  commentCount : number;
+}
+ 
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return{
+      author : 'newjeans_official',
+      title : 'newjeans minji',
+      content : 'hot newjeans',
+      likeCount : 1000000,
+      commentCount : 2000000,
+    };
   }
 }
