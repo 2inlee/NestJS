d7687e1259aae846624befb31e815123d9e73f13
2inlee <kbs@kakao.com>
2023-10-22 20:12:08 +0900

    path add

--- 1_server.js
+++ 1_server.js
@@ -1,5 +1,6 @@
 // import http from 'http'; // ES2015
 const http = require('http');
+const url = require('url');
 
 // localhost -> 127.0.0.1 -> loop back -> 서버를 실행한 컴퓨터
 const host = 'localhost';
@@ -9,8 +10,15 @@ const port = 3000;
 // res => 응답
 
 const server = http.createServer((req, res) => {
+  const path = url.parse(req, url).pathname;
+
+
+  if (path === '/'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Home Page</h1>');
+  }
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.send('<h1>Hello World</h1>');
+  res.end('<h1>Hello World</h1>');
 });
 
 // 서버가 실행됐을 때 실행되는함수 () => {}
