5d80a0a647a16399e9b8461f34bee42e66710a06
2inlee <kbs@kakao.com>
2023-10-22 20:18:48 +0900

    url.parse path

--- 1_server.js
+++ 1_server.js
@@ -10,15 +10,22 @@ const port = 3000;
 // res => 응답
 
 const server = http.createServer((req, res) => {
-  const path = url.parse(req, url).pathname;
+  const path = url.parse(req.url).pathname;
 
 
   if (path === '/'){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<h1>Home Page</h1>');
+  }else if(path === '/post'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Post Page</h1>');
+  }else if(path === '/user'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>User Page</h1>');
+  }else{
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.end('<h1>404 Page Not Found</h1>');
   }
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Hello World</h1>');
 });
 
 // 서버가 실행됐을 때 실행되는함수 () => {}
