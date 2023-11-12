7db17fcb22eaee34c94cbe9960d468e4dba4f254
2inlee <kbs@kakao.com>
2023-11-12 16:59:08 +0900

    user post OneToMany

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,5 +1,6 @@
 import { identity } from "rxjs";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { UsersModel } from "src/users/entities/users.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class PostsModel {
@@ -7,15 +8,18 @@ export class PostsModel {
   @PrimaryGeneratedColumn()
   id : number;
 
-  @Column()
-  author : string;
+  // 1) UsersModel과 연동된다. 외래키 이용
+  // 2) null 불가능
+  @ManyToOne(() => UsersModel, (user) => user.posts,
+  { nullable: false, onDelete: 'CASCADE' })
+  author : UsersModel;
 
   @Column()
   title : string;
 
   @Column()
   content : string;
-
+ㅇ
   @Column()
   likeCount : number;
 
