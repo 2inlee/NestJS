2f71279739d223d9262d2da27037a4b5878c0a45
2inlee <kbs@kakao.com>
2023-11-19 15:14:17 +0900

    class validator error message

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,9 +1,13 @@
 import { IsString } from "class-validator";
 
 export class createPostDto {
-  @IsString()
+  @IsString({
+    message: '제목은 문자열로 입력해주세요.',
+  })
   title: string;
 
-  @IsString()
+  @IsString({
+    message: '내용은 문자열로 입력해주세요.',
+  })
   content: string;
 }
