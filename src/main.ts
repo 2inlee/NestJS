658bad66f9cec14d1452f8a8dd842e814f6ea29f
2inlee <kbs@kakao.com>
2023-11-23 18:24:34 +0900

    edit main.ts trnasformOption:true

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
+    // 임의로 변환하는걸 가능하게 만들어준다. EX) pagination에서 url파라미터를 number로 생각함
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
   }));
 
   await app.listen(3000);
--- src/posts/dto/paginate-post.dto.ts
+++ src/posts/dto/paginate-post.dto.ts
@@ -1,9 +1,12 @@
+import { Type } from "class-transformer";
 import { IsIn, IsNumber, IsOptional } from "class-validator";
 
 export class paginatePostDto{
 
   // 이전 마지막 데이터의 ID
   // 이 프로퍼티에 입력된 ID 보다 높은 ID부터 값을 가져오기 
+
+  // Query는 URL에 있는 값이라서 전부 String으로 들어온다.
   @IsNumber()
   @IsOptional()
   where__id_more_than?: number;
