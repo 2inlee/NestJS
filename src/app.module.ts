8b6db74117e38a600cc631c18a752bd47f89526b
2inlee <kbs@kakao.com>
2023-11-26 17:05:12 +0900

    Static file serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,10 +11,19 @@ import { CommonModule } from './common/common.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { ENV_DB_DATABASE_KEY, ENV_DB_HOST_KEY, ENV_DB_PASSWORD_KEY, ENV_DB_PORT_KEY, ENV_DB_USERNAME_KEY } from './common/const/env-keys.const';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PUBLIC_FOLDER_PATH } from './common/const/path.const';
 
 @Module({
   imports: [
     PostsModule,
+    ServeStaticModule.forRoot({
+      // 4022.png
+      // http://localhost:3000/public/posts/4022.png -> serveRoot 를 추가해서 가능함
+      // http://localhost:3000/posts/4022.png -> serveRoot 를 추가안했으면 이렇게 나옴
+      rootPath: PUBLIC_FOLDER_PATH,
+      serveRoot: '/public',
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
