6b202814054f6c968ac45ca3d503356d85c7d8db
2inlee <kbs@kakao.com>
2023-11-28 20:26:48 +0900

    socket broadcast to socket.to

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,4 +1,4 @@
-import { MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { ConnectedSocket, MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
 
 @WebSocketGateway({
@@ -14,12 +14,27 @@ export class ChatsGateway implements OnGatewayConnection{
     console.log('New connection', socket.id);
   }
 
+  @SubscribeMessage('enter_chat')
+  enterChat(
+    @MessageBody() data: number[],
+    @ConnectedSocket() socket: Socket
+  ){
+    for(const chatId of data){
+      // socket.join();
+      socket.join(chatId.toString());
+    }
+  }
+
   // socket.on('send_message', (message) => { console.log(message)});
-  
   @SubscribeMessage('send_message')
   sendMessage(
-    @MessageBody() message: string,
+    @MessageBody() message: {message: string, chatId: number},
+    @ConnectedSocket() socket: Socket
   ) {
-    this.server.emit('receive_message', 'hello from server');
+    // socket.to - send message to all users in the room except the sender
+    socket.to(message.chatId.toString()).emit('receive_message', message.message);
+
+    // server.in - send message to all users in the room
+    // this.server.in(message.chatId.toString()).emit('receive_message', message.message);
   }
 }
